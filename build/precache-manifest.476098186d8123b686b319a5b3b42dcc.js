00fece97cc1ffca8921cadf2f9be5372e2f9f1fe
Brian Barbour <steelvoltage@gmail.com>
2019-05-18 06:51:29 -0400

    finished project

---
+++ build/precache-manifest.476098186d8123b686b319a5b3b42dcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94486fd013987dcbef7963a77177fe5",
+    "url": "/dad-joke-machine/index.html"
+  },
+  {
+    "revision": "b77f5aee3d3b7fe864c1",
+    "url": "/dad-joke-machine/static/css/main.c090528a.chunk.css"
+  },
+  {
+    "revision": "38626d007d45fdd8be1d",
+    "url": "/dad-joke-machine/static/js/2.a6d49481.chunk.js"
+  },
+  {
+    "revision": "b77f5aee3d3b7fe864c1",
+    "url": "/dad-joke-machine/static/js/main.1c5cb820.chunk.js"
+  },
+  {
+    "revision": "5b3e7488f4a753e00b23",
+    "url": "/dad-joke-machine/static/js/runtime~main.8be0db87.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import ReactFCCTest from "react-fcctest";
+import { FaTwitter } from "react-icons/fa";
+import { FaQuestion } from "react-icons/fa";
 
-function App() {
+const App = () => {
+  const [joke, setJoke] = useState("");
+  const fetchJoke = async () => {
+    const res = await fetch("https://icanhazdadjoke.com/", {
+      headers: { Accept: "application/json" }
+    });
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    const data = await res.json();
+    return data;
+  };
+  const handleQuote = useCallback(() => {
+    fetchJoke().then(data => setJoke(data));
+  }, []);
+  useEffect(() => {
+    handleQuote();
+  }, [handleQuote]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ReactFCCTest />
+      <main>
+        <div className="quote-box" id="quote-box">
+          <h1 id="text">
+            <span className="big-quote">&ldquo;</span>
+            {joke.joke}
+            <span className="big-quote">&rdquo;</span>
+          </h1>
+          <div className="button-row">
+            <button
+              className="button"
+              id="new-quote"
+              onClick={() => handleQuote()}
+            >
+              <FaQuestion style={{ marginRight: "3px" }} />
+              Ask Dad
+            </button>
+            <a
+              className="button"
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?hashtags=dadjokes&related=freecodecamp&text=${
+                joke.joke
+              }`}
+            >
+              <FaTwitter style={{ marginRight: "3px" }} />
+              Tweet
+            </a>
+          </div>
+          <div className="hidey" id="author">
+            {joke.id}
+          </div>
+        </div>
+      </main>
+      <footer>
+        <h1>dadJokes</h1>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
